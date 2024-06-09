45a6d9c0d7022ea191da2bd77cc531bad68dc110
Lucas Paluci <lucas360usa@gmail.com>
2024-06-09 00:39:45 -0300

    almost last version

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 const btn_menu_mobile = document.querySelector("#btn_menu_mobile");
 const menu = document.querySelector(".navigation_menu");
-let largo_pantalla
+
 btn_menu_mobile.addEventListener("click", function(event)
 {
     largo_pantalla = screen.width;
-    if (largo_pantalla <= 1200)
+    if (largo_pantalla <= 850)
     {
         menu.classList.toggle("btn_menu_mobile")
     }
